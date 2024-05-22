88aaf933bb6e3f101ec1b34ad3f4e18091a1db83
devapriyavs <150225899+devx4me@users.noreply.github.com>
2024-05-22 15:32:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// if(5<10){
+//     console.log("5 is less")
+// }
+// else{
+//     console.log("10 is greater")
+// }
+
+// let time=12;
+// if(time<=12){
+//     console.log("good morning")
+// }
+// else if (time>12 && time<=15){
+//     console.log("good afternoon")
+// }
+// else if(time>12 && time>15 && time<19){
+//     console.log("good evening")
+// }
+// else{
+//     console.log("good night")
+// }
+// let a=18;
+// // (a<20)?console.log("a is less than 20"):console.log("a is greater than 20")
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+// let fruits= ["apple","orange","grapes","banana"];
+
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+// let num =[1,2,3,4,5,6];
+// for(i=1;i<=num.length;i++){
+//     if(num[i]%2==0){
+// console.log(`${num[i]} is even`);
+//     }
+//     else{
+//     console.log(`${num[i]} is odd`);
+//     }
+// }
+
+// let values = [1,2,3,4,5,6,7,8,9]
+// sum = 0;
+// for(i=0;i<values.length;i++){
+//     sum = sum +values[i];
+// }
+// console.log(sum);\
+
+// for(let i=1;i<=5;i++){
+//     for(j=1;j<=5;j++){
+//         console.log(`${i} * ${i} = ${i*j}`)
+// }
+// }
+
